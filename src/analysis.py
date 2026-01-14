577302ec4755267182a69f90dcfb8b4aae3410d6
Aleksander Stanuch <aleksanderstanuch@gmail.com>
2026-01-14 23:07:18 +0100

    feat: Add initial image analysis script and update gitignore to exclude common image formats.

--- src/analysis.py
+++ src/analysis.py
@@ -1,21 +1,31 @@
 import cv2
 import os
 
-def provide_folder_name():
-    folder_name = input("Enter the folder name: ")
-    return folder_name
+RAW_PATH = "./data/test_raw/"
+PROCESSED_PATH = "./data/test_processed/"
 
 def load_image(image_name, folder_name):
-    image = cv2.imread(f"./data/test/{folder_name}/{image_name}")
-    return image
+    return cv2.imread(f"{RAW_PATH}{folder_name}/{image_name}")
+
+def save_image(image, image_name, folder_name):
+    output_folder = f"{PROCESSED_PATH}{folder_name}"
+    os.makedirs(output_folder, exist_ok=True)
+
+    image_new_name = image_name.split(".")[0] + "_processed." + image_name.split(".")[1]
+    cv2.imwrite(f"{output_folder}/{image_new_name}", image)
+    print(f"Image saved as {image_new_name}")
 
 def main():
-    folder_name = provide_folder_name()
-    for image_name in os.listdir(f"./data/test/{folder_name}"):
-        image = load_image(image_name, folder_name)
-        cv2.imshow(image_name, image)   
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    experiment_name = str(input("Enter the experiment folder name: "))
+    if not os.path.exists(f"{RAW_PATH}{experiment_name}"):
+        print(f"Folder {experiment_name} does not exist.")
+        return
 
+    for image_name in os.listdir(f"{RAW_PATH}{experiment_name}"):
+        if not os.path.isfile(f"{RAW_PATH}{experiment_name}/{image_name}"):
+            continue
+        image = load_image(image_name, experiment_name)
+        save_image(image, image_name, experiment_name)
+    
 if __name__ == "__main__":
     main()
